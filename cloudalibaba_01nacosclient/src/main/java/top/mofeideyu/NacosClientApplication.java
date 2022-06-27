79536bf5d7494111cabfe003be05cfd57ed1a037
mofeideyu <mofei2914376031@163.com>
2022-06-27 23:55:25 +0800

    添加子项目nacosclient，并将服务注册到nacos

---
+++ cloudalibaba_01nacosclient/src/main/java/top/mofeideyu/NacosClientApplication.java
@@ -0,0 +1,11 @@
+package top.mofeideyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosClientApplication.class, args);
+    }
+}
